dd16bc843156a2d43668995f7716c4e464553b09
Jasmine Kaur <hararjasmine@gmail.com>
2024-05-09 19:28:25 +0530

    final commit

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.set("views",path.join(__dirname,"/views"));
 
 app.use(express.static(path.join(__dirname,"public")));
 
-let post = [
+let posts = [
     {
         id : uuidv4(),
         username : "meenu",
-        profile_pic : "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d29tYW4lMjBwcm9maWxlfGVufDB8fDB8fHww",
+        profile_pic : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEmdqYIBPVwBtH06yccB0qVI3lI4g8azNPmsRrwqMHwQ&s",
         post_pic : "https://pbs.twimg.com/media/EgSPHupXsAISlBW.jpg",
         content : "Feeling Cute ;)"
     },
@@ -45,13 +45,42 @@ app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 });
 
-app.get("/instagram",(req,res)=>{
-    res.render("index.ejs",{post});
+app.get("/instagram",(req,res)=>{   //rendering a base file which has all the structure
+    res.render("index.ejs",{posts});
 })
 
-app.get("/instagram/:username",(req,res)=>{
-    let {username} = req.params;
-    res.render("index.ejs",{username});
+app.get("/instagram/new",(req,res)=>{ //opening a form to get the details of the new post
+    res.render("new.ejs");
 })
+
+app.post("/instagram",(req,res)=>{ //adding the new post to the main page
+    let {username,content,post_pic,profile_pic} = req.body;
+    let id = uuidv4();
+    posts.push({id,username,content,post_pic,profile_pic}); 
+    res.redirect("/instagram");
+})
+
+app.delete("/instagram/:id",(req,res)=>{   //delete any post
+    let {id} = req.params;
+    posts = posts.filter((p)=> id !== p.id);
+    res.redirect("/instagram");
+
+})
+
+app.get("/instagram/:id/edit",(req,res)=>{  //edit post using form
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id)
+    res.render("edit.ejs",{post});
+})
+
+app.patch("/instagram/:id",(req,res)=>{   //updating the post
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/instagram");
+})
+
 
 
